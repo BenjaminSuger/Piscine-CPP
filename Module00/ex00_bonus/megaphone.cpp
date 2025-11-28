d8bc4678bce150943b7b9669ba7caef4f114ebdb
sugerbenjamin@prontonmail.com <sugerbenjamin@prontonmail.com>
2025-11-29 00:19:15 +0100

    bonus done for Module00 ex00 => demonstration of 2 different loop mecanism

--- Module00/ex00_bonus/megaphone.cpp
+++ Module00/ex00_bonus/megaphone.cpp
@@ -6,20 +6,29 @@
 /*   By: bsuger <bsuger@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/07 03:35:49 by bsuger            #+#    #+#             */
-/*   Updated: 2025/11/07 03:35:54 by bsuger           ###   ########.fr       */
+/*   Updated: 2025/11/29 00:18:20 by bsuger           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <cctype>
+#include <algorithm>
 
+//demonstration of std::transform
 std::string	Shout(std::string arg)
 {
-	for (long unsigned int i = 0; i < arg.size(); i++)
-		arg[i] = std::toupper(arg[i]);
+	std::transform(arg.begin(), arg.end(), arg.begin(), ::toupper);
 	return (arg);
 }
 
+/* demonstration for range loop
+std::string	Shout(std::string arg)
+{
+	for (auto &it : arg)
+		it = std::toupper(it);
+	return (arg);
+}
+*/
 int	main(int argc, char **argv)
 {
 	if (argc == 1)
