6292f20a03c11cff5a7ab892c994f7128235d1c6
Benjamin Suger <bsuger@f6r5s16.clusters.42paris.fr>
2025-12-29 16:27:56 +0100

    start the ex02

---
+++ Module08/ex02/includes/MutantStack.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsuger <bsuger@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/29 16:14:03 by bsuger            #+#    #+#             */
+/*   Updated: 2025/12/29 16:15:58 by bsuger           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <stack>
+
+template <typename T>
+class MutantStack : public std::stack {
+	private:
+
+
+	public:
+
+
+
+
+
+
+};
+
+
+#endif
